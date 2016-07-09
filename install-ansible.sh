5a98e216ec85dc1c018b5e3ee1d13a1282ce13a4
Gaël-Ian Havard <gaelian.havard@gmail.com>
2016-07-09 13:25:02 +0200

    Add rsync to the list of packages installed on manager

--- install-ansible.sh
+++ install-ansible.sh
@@ -7,7 +7,7 @@ apt-get -qq update > /dev/null
 apt-get -qq upgrade > /dev/null
 
 echo "Installing Ansible dependencies"
-apt-get -qq install build-essential libssl-dev libffi-dev python-dev python-pip sshpass > /dev/null
+apt-get -qq install build-essential libssl-dev libffi-dev python-dev python-pip sshpass rsync > /dev/null
 
 echo "Installing Ansible"
 pip install -qq --upgrade ansible
