b94e9701eefabf45801506e6386f44c758d28a62
Gaël-Ian Havard <gaelian.havard@gmail.com>
2018-06-28 11:00:02 +0200

    Switch to Ansible official PPA repository to install Ansible

--- install-ansible.sh
+++ install-ansible.sh
@@ -2,15 +2,21 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+
 echo "Updating system"
 apt-get -qq update > /dev/null
 apt-get -qq upgrade > /dev/null
 
-echo "Installing Ansible dependencies"
-apt-get -qq install build-essential libssl-dev libffi-dev python-dev python-pip sshpass rsync > /dev/null
+
+echo "Installing Ansible official repository"
+apt-get -qq install dirmngr > /dev/null
+echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' > /etc/apt/sources.list.d/ansible.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367 2>/dev/null
+apt-get -qq update > /dev/null
+
 
 echo "Installing Ansible"
-pip install -qq --upgrade pip setuptools jmespath
-pip install -qq --upgrade ansible
+apt-get -qq install ansible python-jmespath sshpass rsync > /dev/null
+
 
 unset DEBIAN_FRONTEND
